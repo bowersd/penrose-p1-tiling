d5b2f3a11057c26aa468d9d5c88d6c81d1736fd4
sautedman <bowersd@email.arizona.edu>
2023-03-18 20:56:31 -0700

    sketched function

--- subdivision.py
+++ subdivision.py
@@ -4,3 +4,14 @@ import math
 #these have 5, 10, 4, 7 vertices, respectively, so that is how we will identify them
 
 phi = (1 + math.sqrt(5))/2
+
+def subdivide(*shapes):
+    for s in shapes:
+        if len(s) == 4:
+            pass #rhombus -> 1 pentagon (and join with neighboring pentagons to make 1 boat and 1 pentagram)
+        if len(s) == 5:
+            pass #pentagon -> 6 pentagons (and join with neighboring pentagons to make 5 rhombuses)
+        if len(s) == 7:
+            pass #boat -> 3 pentagons (and join with neighboring pentagons to make 3 boats on tips and a pentagram on base)
+        if len(s) == 10:
+            pass #pentagram -> 5 pentagons, 1 pentagram (and join with neighboring pentagons to make 5 boats on tips)
