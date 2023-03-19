787ba9bdb77d02d9107f93561d35f139ddfdc223
sautedman <bowersd@email.arizona.edu>
2023-03-18 20:50:23 -0700

    sketched out main strategy in comment

---
+++ subdivision.py
@@ -0,0 +1,5 @@
+import math
+
+#p1 tiling uses 4 tiles: pentagons, pentagrams (5 pointed star), rhombuses, and boats (roughly a 5 pointed star with 2 points lopped off)
+#these have 5, 10, 4, 7 vertices, respectively, so that is how we will identify them
+
