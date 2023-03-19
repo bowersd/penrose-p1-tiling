b0004e49becbe6717aa0645eb0bcc3828cfd6e38
sautedman <bowersd@email.arizona.edu>
2023-03-18 22:00:20 -0700

    notes to treat pentagons as triangles (makes creation of partial shapes on edges be well defined)

--- subdivision.py
+++ subdivision.py
@@ -2,6 +2,9 @@ import math
 
 #p1 tiling uses 4 tiles: pentagons, pentagrams (5 pointed star), rhombuses, and boats (roughly a 5 pointed star with 2 points lopped off)
 #these have 5, 10, 4, 7 vertices, respectively, so that is how we will identify them
+            #pentagon = 2 108x36x36 triangles + 1 36x72x72 triangle, 
+            #so break the 108 triangles into a pentagon thus composed flanked by a pair of 36 (joins to make rhombus) and 108 triangles
+            #break the 36 triangle into a pentagon thus composed, plus 3 36s (finishing pentagons), 2 108s (finishing pentagons), and a 36 (joins to make a rhombus)
 
 phi = (1 + math.sqrt(5))/2
 
@@ -13,7 +16,7 @@ def subdivide(depth, *shapes):
         if len(s) == 4:
             pass #rhombus -> 1 pentagon (and join with neighboring pentagons to make 1 boat and 1 pentagram)
         if len(s) == 5:
-            pass #pentagon -> 6 pentagons (and join with neighboring pentagons to make 5 rhombuses)
+            pass #pentagon -> 6 pentagons (and join with neighboring pentagons to make 5 rhombuses) 
         if len(s) == 7:
             pass #boat -> 3 pentagons (and join with neighboring pentagons to make 3 boats on tips and a pentagram on base)
         if len(s) == 10:
