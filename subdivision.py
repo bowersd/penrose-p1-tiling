fad561294c7cf5b808c1bce3726b52ddb506e2d0
sautedman <bowersd@email.arizona.edu>
2023-03-18 20:51:31 -0700

    defined phi

--- subdivision.py
+++ subdivision.py
@@ -3,3 +3,4 @@ import math
 #p1 tiling uses 4 tiles: pentagons, pentagrams (5 pointed star), rhombuses, and boats (roughly a 5 pointed star with 2 points lopped off)
 #these have 5, 10, 4, 7 vertices, respectively, so that is how we will identify them
 
+phi = (1 + math.sqrt(5))/2
